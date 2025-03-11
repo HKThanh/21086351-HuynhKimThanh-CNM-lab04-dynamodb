5c642aa8914427f5feaee960dbe11441c534ba95
Huỳnh Kim Thành <121759693+HKThanh@users.noreply.github.com>
2025-03-11 14:22:21 +0700

    add read function

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+require('dotenv').config('/.env');
+const app = express();
+const PORT = 3000;
+
+const AWS = require('aws-sdk');
+AWS.config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+
+// register middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./views'));
+
+//config view
+app.set('view engine', 'ejs'); //Khai báo web sẽ dùng engine ejs để render
+app.set('views', './views'); // Nội dung render nằm trong thư mục views
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+const tableName = 'Subject';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+app.get('/', (req, res) => {
+    const params = {
+        TableName: tableName
+    };
+
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(data.Items);
+            res.render('index', { data: data.Items });
+        }
+    });
+});
